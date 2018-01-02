056151066685c957a7ff077c902b94375789b47f
Mike Yoniles <mike@seneca.hsd1.in.comcast.net>
2018-01-02 13:56:09 -0500

    init of branch

--- ff.py
+++ ff.py
@@ -8,72 +8,76 @@ def sigmoid(value):
 def dsigmoid(value):
     return value * (1 - value)
 
-# This fuction is from the data file and produces an array of 8-bit representations of numbers
-X, Y = make_data_set()
-X = np.array(X)
-Y = np.array(Y)
+
+class ff:
+
+    def __init__(num_layers, layer_dim, output_dim, epochs, learning_rate):
+        # This fuction is from the data file and produces an array of 8-bit representations of numbers
+        X, Y = make_data_set()
+        self.X = np.array(X)
+        self.Y = np.array(Y)
+
+        ### NOTICE THAT BIAS HAS NOT BEEN IMPLEMENTED
+        ### initializes random weights for the net to produce a single probability
+        #bias = 1
+        self.W_i2h = 2* np.random.random((8,4)) -1
+        self.W_h2o = 2* np.random.random((4,1)) -1
+        self.learning_rate = learning_rate
 
 
-### NOTICE THAT BIAS HAS NOT BEEN IMPLEMENTED
-### initializes random weights for the net to produce a single probability
-#bias = 1
-W_i2h = 2* np.random.random((8,4)) -1
-W_h2o = 2* np.random.random((4,1)) -1
-learning_rate = 0.01
+    def train(self):
+        for i in range ( self.epochs ):
+            # This is what trains the nueral net and updates the weights
+            inputV = self.X
+            # forward pass
+            unsq_h = np.dot(inputV, self.W_i2h) 
+            sqh = sigmoid(unsq_h)
 
+            unsq_o = sqh.dot(self.W_h2o) 
+            sqo = sigmoid(unsq_o)
 
-for i in range ( 150000 ):
-    # This is what trains the nueral net and updates the weights
-    inputV = X
-    # forward pass
-    unsq_h = np.dot(inputV, W_i2h) 
-    sqh = sigmoid(unsq_h)
+            #find loss
+            loss = Y - sqo
 
-    unsq_o = sqh.dot(W_h2o) 
-    sqo = sigmoid(unsq_o)
+            #for documentation purposes
+            if ( i % 2000 == 0 ):
+                print ("Error:", str(np.mean(np.abs(loss))), "\r", end ="")
 
-    #find loss
-    loss = Y - sqo
+            #find deltas
+            dW_h2o = loss * dsigmoid(sqo)
 
-    #for documentation purposes
-    if ( i % 2000 == 0 ):
-        print ("Error:", str(np.mean(np.abs(loss))), "\r", end ="")
+            dW_i2h = (dW_h2o.dot(W_h2o.T)) * dsigmoid(sqh)
 
-    #find deltas
-    dW_h2o = loss * dsigmoid(sqo)
+            #Update the weights
+            self.W_i2h += inputV.T.dot(dW_i2h) * learning_rate
+            self.W_h2o += sqh.T.dot(dW_h2o) * learning_rate
+        print("\n")
 
-    dW_i2h = (dW_h2o.dot(W_h2o.T)) * dsigmoid(sqh)
+    def predict(self, int_G):
+        #This just does a forward pass and returns the squashed value of the output layer
+        #its a repeat of the first half of the loop
+        x = to_vector(int_G)
+        unsq_h = np.dot(x, self.W_i2h) 
+        sqh = sigmoid(unsq_h)
 
-    #Update the weights
-    W_i2h += inputV.T.dot(dW_i2h) * learning_rate
-    W_h2o += sqh.T.dot(dW_h2o) * learning_rate
-print("\n")
+        unsq_o = sqh.dot(self.W_h2o)
+        sqo = sigmoid(unsq_o)
+        return sqo[0]
 
-def predict(int_G):
-    #This just does a forward pass and returns the squashed value of the output layer
-    #its a repeat of the first half of the loop
-    x = toBinary(int_G)
-    unsq_h = np.dot(x, W_i2h) 
-    sqh = sigmoid(unsq_h)
-
-    unsq_o = sqh.dot(W_h2o)
-    sqo = sigmoid(unsq_o)
-    return sqo[0]
-
-
-testing = True
-while(testing):
-    testInput = input("Enter a number ")
-    testInput = int(testInput)
-    if(testInput >= 0 and testInput < 256 ):
-        if(predict(testInput) >= 0.5):
-            heck = "yes"
-        else:
-            heck = "no"
-        print("The probability that" , testInput , "is divisible by three is" , predict(testInput))
-        print("In other words:" , heck)
-    else:
-        testing = False
+    def test(self):
+        testing = True
+        while(testing):
+            testInput = input("Enter a number ")
+            testInput = int(testInput)
+            if(testInput >= 0 and testInput < 256 ):
+                if(predict(testInput) >= 0.5):
+                    heck = "yes"
+                else:
+                    heck = "no"
+                print("The probability that" , testInput , "is divisible by three is" , predict(testInput))
+                print("In other words:" , heck)
+            else:
+                testing = False
 
 
 
